79cee323f5760dfff9735b1e20b17e9ad0f7ca1a
Sam Day <me@samcday.com>
2021-08-24 00:20:12 +0200

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ use tracing_subscriber::EnvFilter;
 
 const CONSOLE_URL: &str = "https://console.hetzner.cloud";
 
-use serde::{Serialize, Deserialize};
 use eyre::{eyre, Result};
-use std::collections::HashMap;
 use headless_chrome::{Browser, LaunchOptionsBuilder};
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct CloudConsoleToken {
@@ -15,7 +15,9 @@ struct CloudConsoleToken {
 #[tokio::main]
 async fn main() -> Result<()> {
     color_eyre::install()?;
-    tracing_subscriber::fmt::fmt().with_env_filter(EnvFilter::from_default_env()).init();
+    tracing_subscriber::fmt::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
     //
     // let matches = App::new("hcloud-project-manager")
     //     .version(crate_version!())
@@ -27,18 +29,27 @@ async fn main() -> Result<()> {
 
     let raw_token = get_user_token(&username, &password).map_err(|e| e.compat())?;
 
-    let token_str = form_urlencoded::parse(("v=".to_owned()+&raw_token).as_bytes())
-        .next().ok_or_else(|| eyre!("couldn't parse url-encoded token"))?.1.into_owned();
+    let token_str = form_urlencoded::parse(("v=".to_owned() + &raw_token).as_bytes())
+        .next()
+        .ok_or_else(|| eyre!("couldn't parse url-encoded token"))?
+        .1
+        .into_owned();
 
     let token = serde_json::from_str::<HashMap<String, CloudConsoleToken>>(&token_str)?
-        .values().next().ok_or_else(|| eyre!("couldn't locate token in cookie"))?.token.to_owned();
+        .values()
+        .next()
+        .ok_or_else(|| eyre!("couldn't locate token in cookie"))?
+        .token
+        .to_owned();
 
     println!("{}", token);
 
     let client = reqwest::Client::new();
-    let response = client.get("https://api.hetzner.cloud/v1/_projects")
+    let response = client
+        .get("https://api.hetzner.cloud/v1/_projects")
         .bearer_auth(token)
-        .send().await?;
+        .send()
+        .await?;
 
     println!("{}", response.text().await?);
 
@@ -48,15 +59,29 @@ async fn main() -> Result<()> {
 // Uses a headless browser and provided login credentials to obtain a Hetzner API user token.
 // This token is permitted to create/delete projects, and obtain API project_user tokens.
 fn get_user_token(username: &str, password: &str) -> Result<String, failure::Error> {
-    let browser = Browser::new(LaunchOptionsBuilder::default()
-        .path(Some("/headless-shell/headless-shell".into()))
-        .sandbox(false)
-        .build().or_else(|err| Err(failure::format_err!("failed to launch headless_chrome: {}", err)))?)?;
+    let browser = Browser::new(
+        LaunchOptionsBuilder::default()
+            .path(Some("/headless-shell/headless-shell".into()))
+            .sandbox(false)
+            .build()
+            .or_else(|err| {
+                Err(failure::format_err!(
+                    "failed to launch headless_chrome: {}",
+                    err
+                ))
+            })?,
+    )?;
     let tab = browser.wait_for_initial_tab()?;
     tab.navigate_to(CONSOLE_URL)?;
     tab.wait_until_navigated()?;
-    if !tab.get_url().starts_with("https://accounts.hetzner.com/login") {
-        return Err(failure::format_err!("expected navigation to login page, got {} instead", tab.get_url()));
+    if !tab
+        .get_url()
+        .starts_with("https://accounts.hetzner.com/login")
+    {
+        return Err(failure::format_err!(
+            "expected navigation to login page, got {} instead",
+            tab.get_url()
+        ));
     }
 
     tab.wait_for_element("#_username")?.type_into(username)?;
@@ -65,19 +90,11 @@ fn get_user_token(username: &str, password: &str) -> Result<String, failure::Err
 
     tab.wait_for_element(".user-details__robotcn")?;
 
-    Ok(tab.get_cookies()?.iter()
+    Ok(tab
+        .get_cookies()?
+        .iter()
         .find(|cookie| cookie.name == "tokens" && cookie.domain == "console.hetzner.cloud")
         .ok_or(failure::format_err!("Couldn't find tokens cookie"))?
-        .value.to_owned()
-    )
-
-    // for cookie in tab.get_cookies()? {
-    //     println!("{:?}", cookie);
-    // }
-    //
-    // let png = tab.capture_screenshot(ScreenshotFormat::PNG, None, true)?;
-    // let mut f = File::create("/tmp/test.png")?;
-    // f.write_all(&png)?;
-    //
-    // Ok("hi mom".to_string())
+        .value
+        .to_owned())
 }
