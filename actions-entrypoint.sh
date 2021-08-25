5e91467033ff0712d49257109ae627a8797cf76d
Sam Day <me@samcday.com>
2021-08-25 13:43:16 +0200

    actually set token in action

--- actions-entrypoint.sh
+++ actions-entrypoint.sh
@@ -5,19 +5,16 @@ HCLOUD_USER_TOKEN=$(hcloud-project-manager login)
 echo "::add-mask::$HCLOUD_USER_TOKEN"
 export HCLOUD_USER_TOKEN
 
-echo "got user token $HCLOUD_USER_TOKEN from $HETZNER_USERNAME and $HETZNER_PASSWORD"
-
 if [[ "$2" == "create" ]]; then
   project_id=$(hcloud-project-manager create "$1")
   token=$(hcloud-project-manager token "$1")
   echo "::add-mask::$token"
   echo "::set-output name=project_id::$project_id"
   echo "::set-output name=token::$token"
-
-  echo "project token is $token"
+  echo "HCLOUD_PROJECT_ID=$project_id" >> "$GITHUB_ENV"
 
   if [[ "$SET_TOKEN" == "true" ]]; then
-    echo "HCLOUD_PROJECT_ID=$project_id" >> "$GITHUB_ENV"
+   echo "HCLOUD_TOKEN=$token" >> "$GITHUB_ENV"
   fi
 elif [[ "$2" == "delete" ]]; then
   hcloud-project-manager delete "$1"
